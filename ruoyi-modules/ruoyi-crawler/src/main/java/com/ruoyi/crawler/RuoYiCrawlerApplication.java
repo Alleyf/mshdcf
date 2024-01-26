87572bc53abfb2d5195a7b2ce7ecea6d71604f1a
Alleyf <3035581811@qq.com>
2024-01-26 14:47:41 +0800

    添加采集模块，完成crawlab的node节点管理（增删改查）接口功能。

---
+++ ruoyi-modules/ruoyi-crawler/src/main/java/com/ruoyi/crawler/RuoYiCrawlerApplication.java
@@ -0,0 +1,24 @@
+package com.ruoyi.crawler;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author fcs
+ * @date 2024/1/23 14:50
+ * @site <a href="https://alleyf.github.io">getHelp</a>
+ */
+@SpringBootApplication
+@EnableFeignClients
+@EnableDubbo
+public class RuoYiCrawlerApplication {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(RuoYiCrawlerApplication.class);
+        application.setApplicationStartup(new BufferingApplicationStartup(2048));
+        application.run(args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  爬虫服务模块启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
